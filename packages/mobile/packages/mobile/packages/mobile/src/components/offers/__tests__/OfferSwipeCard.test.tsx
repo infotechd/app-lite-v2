8a2c6857ebdcdfa3582436f8c187ed7d1ec4fe36
infotechd <app.infotechdigital@gmail.com>
2026-02-10 19:00:24 -0300

    test/offer-card-media-gallery: testes para a galeria de midia em um offer card

---
+++ packages/mobile/packages/mobile/packages/mobile/src/components/offers/__tests__/OfferSwipeCard.test.tsx
@@ -0,0 +1,6 @@
+// Arquivo duplicado neutralizado por solicitação do usuário.
+describe('DUPLICATE OfferSwipeCard.test.tsx (aninhado 2x)', () => {
+  it('noop', () => {
+    expect(true).toBe(true);
+  });
+});
